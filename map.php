088f6a94b706228c9057b8fae4fde5d388aa55ed
jawad zafari <jawad.zafari@laplateforme.io>
2025-12-03 11:32:37 +0100

    change

--- map.php
+++ map.php
@@ -5,10 +5,7 @@ regles_homemade_warhammer_mvc/
 │   └──database.php
 │
 ├── controllers/
-│   ├──admin_controller.php
 │   ├──auth_controller.php
-│   ├──catalog_controller.php
-│   ├──products_controller.php
 |   └──home_controller.php
 |
 ├── core/
@@ -23,7 +20,7 @@ regles_homemade_warhammer_mvc/
 │   └──helpers.php
 |
 ├── models/
-|    ├──.php
+|    ├──products_model.php
 |    └──user_model.php
 |
 ├── public/
@@ -37,37 +34,19 @@ regles_homemade_warhammer_mvc/
 |    └──index.php
 |
 ├── views/
-│   ├── admin/
-|   |   ├──dashboard.php    
-|   |   ├──.php    
-|   |   ├──.php    
-|   |   ├──.php    
-|   |   ├──user_detail.php    
-|   |   └──users_list.php
 │   ├── auth/
 |   |   ├──login.php    
 |   |   └──register.php
-│   ├── catalog/
-|   |   ├──products.php   
-|   |   ├──.php   
-|   |   ├──.php   
-|   |   └──index.php
 |   |  
 |   ├──errors/
 |   |   └──404.php
 |   ├──home/  
 |   |   ├──about.php
-|   |   ├──.php
 |   |   ├──contact.php
 |   |   ├──index.php
 |   |   ├──profile.php
 |   |   └──test.php
-|   ├──layouts/  
-|   |  └──layouts.php
-|
-├── .gitignore
-├──CHANGELOG.md
-├──CODING_STANDARDS.md
-├──CODING_STANDARDS.pdf
-├──map.php
+|   └──layouts/  
+|       └──layouts.php
+└──map.php
 </code></pre>
