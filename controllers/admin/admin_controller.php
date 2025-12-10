31e0e17f2ce7328e3ee34e04d02404cdc92fe0ca
pierre-canet <pierre.canet@laplateforme.io>
2025-12-10 16:56:27 +0100

    Affichage des produits dans l'interface administrateur

--- controllers/admin/admin_controller.php
+++ controllers/admin/admin_controller.php
@@ -1,6 +1,22 @@
 <?php
+require_admin();
 function admin_index()
 {
     $data = [];
+
     load_view_with_layout('admin/index', $data, 'admin_layout');
+}
+function admin_product_management()
+{
+    $products = get_all_products();
+    foreach ($products as &$product) {
+        $product['price'] = get_product_price($product['id']);
+    }
+    unset($product);
+    $data = [
+        'title' => 'gestion produits',
+        'products' => $products,
+    ];
+
+    load_view_with_layout('admin/product_management', $data, 'admin_layout');
 }
---
+++ views/admin/product_management.php
@@ -0,0 +1,22 @@
+<table>
+    <tr>
+        <th>nom</th>
+        <th>Description</th>
+        <th>Image</th>
+        <th>Prix</th>
+        <th>Catégorie</th>
+        <th>Stock</th>
+    </tr>
+    <?php foreach ($products as $p) : ?>
+        <tr>
+            <td><?php e($p['name']) ?></td>
+            <td><?php e($p['description']) ?></td>
+            <td><img src="<?php echo url($p['product_image']); ?>"
+                    alt="<?= e($p['name']) ?>"
+                    onerror="this.src='<?php echo url('assets/images/default.jpg'); ?>'" height="50px" width="auto"></td>
+            <td><?php e($p['price']) ?></td>
+            <td></td>
+            <td></td>
+        </tr>
+    <?php endforeach ?>
+</table>
