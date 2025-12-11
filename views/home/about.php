7cb368f24fa95d96b971e9a4648497e5ead09c2c
pierre-canet <pierre.canet@laplateforme.io>
2025-12-11 22:38:37 +0100

    Ajout de la page Catalogue complète

--- views/home/about.php
+++ views/home/about.php
@@ -8,7 +8,7 @@
     <div class="container">
         <div class="content-grid">
             <div class="content-main">
-                <h2>À propos de cette application</h2>
+                <h2>À propos de notre site</h2>
                 <p><?php e($content); ?></p>
 
                 <h3>Un catalogue immense !</h3>
@@ -20,40 +20,51 @@
                         fête, un anniversaire ou juste pour une envie de sucreries !
                     </li>
                     <li>
-                        <strong>Confiseries adaptées</strong> : sucreries végan, confiseries Halal,
-                        bobons sans sucre, tout est là pour permettre à chacun de profiter de
-                        confiseries adaptées à son mode vie
-                    </li>
-                    <li>
-                        <strong>Confiseries d'antan</strong> : Car en sac, rondoudous, berlingots,
-                        les confiseries favorites de nos parents et de nos grands-parents se sont
-                        données rendez-vous dans notre boutique pour vous permettre de partager
-                        d'inoubliables moments en famille !
-                    </li>
-                    <li>
                         <strong>Chocolats</strong> : tablettes, chocolats fourrés à la liqueur ou non,
                         chocolats de fêtes, chocolat noir, noir intense, au lait ou blanc, les fans
                         du cacao trouveront leur bonheur chez nous !
                     </li>
+                    <li><strong>Guimauves</strong> : Avis à tous les fadas de guimauves, vous êtes les
+                        bienvenus dans nos boutiques qui vous proposerons tout ce que vous pourriez
+                        souhaiter !
+                    </li>
+                    <li><strong>Sucettes</strong> : Vous préférez les sucreries qui tiennent plus
+                        longtemps, qui se savourent ? Alors n'ayez crainte, nous vous proposons la
+                        meilleure gamme de sucettes !
+                    </li>
+                </ul>
+                <h3>Catégories à venir</h3>
+                <ul>
+                    <li>
+                        <strong>Confiseries adaptées</strong> : sucreries végan, confiseries Halal,
+                        bobons sans sucre, tout sera là pour permettre à chacun de profiter de
+                        confiseries adaptées à son mode vie
+                    </li>
+                    <li>
+                        <strong>Confiseries d'antan</strong> : Car en sac, rondoudous, berlingots,
+                        les confiseries favorites de nos parents et de nos grands-parents se donneront
+                        bientôt rendez-vous dans notre boutique pour vous permettre de partager
+                        d'inoubliables moments en famille !
+                    </li>
                     <li>
                         <strong>Boissons</strong> : Sodas, boissons énergisantes, thés glacés, boissons
-                        aux fruits, Cia Kombucha, si vous avez une petite soif, vous trouverez ici de
-                        quoi l'étancher.
+                        aux fruits, Cia Kombucha, si vous avez une petite soif, vous trouverez sous peu
+                        ici de quoi l'étancher.
                     </li>
                     <li>
                         <strong>Box personnalisée</strong> : Vous avez envie de plusieurs confiseries
-                        différentes en même temps ? Alors composez vous-même la box de vos rêves à des
-                        prix avantageux !
+                        différentes en même temps ? Alors dans le futur vous pourrez composer vous-même
+                        la box de vos rêves à des prix avantageux !
                     </li>
                 </ul>
                 <h3>Nos fondateurs</h3>
-                <p>Mais la conception de ce site n'aurait pas pu être possible sans la collaboration de :</p>
+                <p>La conception de ce site n'aurait pas pu être possible sans la collaboration de :</p>
                 <ul>
                     <li><strong>Jawad</strong> : Designer et concepteur de notre catalogue !</li>
-                    <li><strong>Mehdi</strong> : Développeur de notre page d'accueil</li>
-                    <li><strong>Adam</strong> : Développeur de notre page profil !</li>
+                    <li><strong>Mehdi</strong> : Développeur de notre page d'accueil et rédacteur des mentions légales</li>
+                    <li><strong>Adam</strong> : Développeur de notre page profil et du panier !</li>
                     <li><strong>Pierre</strong> : Concepteur du back-office et des pages connexion et inscription</li>
-                    <li><strong>Ivan</strong> : le Designer des maquettes du site !</li>
+                    <li><strong>Ivan</strong> : le Designer des maquettes du site et le responsable du passage en sous-domaines !</li>
                 </ul>
 
                 <h3>Qui sommes-nous ?</h3>
